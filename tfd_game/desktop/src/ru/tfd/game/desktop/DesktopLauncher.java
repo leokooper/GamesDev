e21241c18c0680148879817253fc6eac9fdd1ba3
leokooper <lverixas@gmail.com>
2018-05-08 13:26:29 +0300

    //games relocated from another repository

---
+++ tfd_game/desktop/src/ru/tfd/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.tfd.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.tfd.game.TFDGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TFDGame(), config);
+	}
+}
